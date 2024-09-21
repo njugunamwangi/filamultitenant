6a05bbd31fafb018ff164283a6f0c1d859cb11a1
njugunamwangi <njugunamwangi.n@gmail.com>
2024-09-21 15:55:08 +0300

    Manager panel

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/css/filament/admin/theme.css',
                 'resources/css/filament/company/theme.css',
+                'resources/css/filament/manager/theme.css',
             ],
             refresh: true,
         }),
